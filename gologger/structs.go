23d0e6ab9e3c4cfda3e4b25f553b5c28d5994b58
mitchya1 <mitchell.anderson@colorado.edu>
2020-06-25 08:57:40 -0600

    add comment

--- gologger/structs.go
+++ gologger/structs.go
@@ -1,5 +1,6 @@
 package gologger
 
+// LoggerConfig has the graylog server hostname (e.g. logs.example.com) and the input port (e.g. 12201)
 type LoggerConfig struct {
 	GraylogHostname string
 	GraylogPort     int
